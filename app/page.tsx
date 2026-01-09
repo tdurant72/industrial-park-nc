b2f23e30068a184ccbff6cd3631d809ef262f425
Tyus Durant <todurant@ncdot.gov>
2026-01-09 14:20:23 -0500

    init commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,128 @@
-import Image from "next/image";
 
-export default function Home() {
-  return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+import { ReactLenis } from 'lenis/react'
+import { Metadata } from "next";
+import ShaderBackground from "@/components/ui/shader-background2";
+import Hero from "@/components/sections/Hero";
+import BusinessAdvantages from "@/components/sections/business-advantages";
+import Infrastructure from "@/components/sections/infrastructure";
+import { Partners } from '@/components/sections/partners';
+import Latest from "@/components/sections/latest";
+import Faq from "@/components/sections/faq";
+import Process from "@/components/sections/process";
+import StickyExpertButton from "@/components/sticky-expert-button";
+import ConciergeRelocationBlurb from "@/components/concierge-relocation-blurb";
+import MapSection from "@/components/sections/map-section";
+export const metadata: Metadata = {
+  title: "NCGTP - North Carolina Global TransPark",
+  description: "Discover the North Carolina Global TransPark (NCGTP), a premier logistics and aerospace industrial park designed to accelerate business growth and innovation. Explore our state-of-the-art infrastructure, strategic location, and business advantages that make NCGTP the ideal destination for your next venture.",
+  keywords: [
+    "NCGTP",
+    "North Carolina Global TransPark",
+    "logistics hub",
+    "aerospace park",
+    "industrial park",
+    "business growth",
+    "infrastructure",
+    "site selection",
+    "business incentives",
+    "supply chain solutions",
+    "manufacturing",
+    "distribution center",
+    "transportation network",
+    "economic development",
+    "Eastern North Carolina"
+  ],
+  openGraph: {
+    title: "NCGTP - North Carolina Global TransPark",
+    description: "Discover the North Carolina Global TransPark (NCGTP), a premier logistics and aerospace industrial park designed to accelerate business growth and innovation. Explore our state-of-the-art infrastructure, strategic location, and business advantages that make NCGTP the ideal destination for your next venture.",
+    url: "https://www.ncgtp.com/",
+    siteName: "NCGTP",
+    images: [
+      {
+        url: "https://www.ncgtp.com/og-image-ncgtp.png",
+        width: 1200,
+        height: 630,
+        alt: "NCGTP - North Carolina Global TransPark",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  robots: {
+    index: true,
+    follow: true,
+    nocache: true,
+    googleBot: {
+      index: true,
+      follow: true,
+      "max-video-preview": -1,
+      "max-image-preview": "large",
+      "max-snippet": -1,
+    },
+  },
+  alternates: {
+    canonical: "https://www.ncgtp.com/",
+  }
 }
+const structuredData = {
+  "@context": "https://schema.org",
+  "@type": "WebSite",
+  "name": "NCGTP - North Carolina Global TransPark",
+  "url": "https://www.ncgtp.com/",
+  "description": "Discover the North Carolina Global TransPark (NCGTP), a premier logistics and aerospace industrial park designed to accelerate business growth and innovation. Explore our state-of-the-art infrastructure, strategic location, and business advantages that make NCGTP the ideal destination for your next venture.",
+  "streetAddress": "2711 Gardner Rd",
+  "addressLocality": "Kinston",
+  "addressRegion": "NC",
+  "postalCode": "28504",
+  "addressCountry": "USA",
+  "image": "https://www.ncgtp.com/og-image-ncgtp.png",
+  "keywords": [
+    "NCGTP",
+    "North Carolina Global TransPark",
+    "logistics hub",
+    "aerospace park",
+    "industrial park",
+    "business growth",
+    "infrastructure",
+    "site selection",
+    "business incentives",
+    "supply chain solutions",
+    "manufacturing",
+    "distribution center",
+    "transportation network",
+    "economic development",
+    "Eastern North Carolina"
+  ],
+  "publisher": {
+    "@type": "Organization",
+    "name": "NCGTP",
+    "url": "https://www.ncgtp.com/",
+    "logo": {
+      "@type": "ImageObject",
+      "url": "https://www.ncgtp.com/og-image-ncgtp.png"
+    }
+  }
+};
+const Home = () => {
+  return (<div>
+    <ReactLenis root>
+      <Hero />
+      <BusinessAdvantages />
+      <Infrastructure />
+      <ConciergeRelocationBlurb />
+      {/* <ParallaxDivider /> */}
+      <Process />
+      <Partners />
+      {/* <SiteSelection /> */}
+      {/* <SiteSelectionAlt /> */}
+      <MapSection />
+      <Latest />
+      <Faq />
+      {/* background graphic webgl */}
+      <ShaderBackground />
+      <StickyExpertButton />
+    </ReactLenis>
+  </div>);
+}
+
+export default Home;
--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,13 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "admin.zoomprospector.com",
+      "properties.zoomprospector.com",
+      // add any other domains you use for images
+    ],
+  },
 };
 
 export default nextConfig;
