52e28c77ffa6a5a17f6fb7240abf6c974f268203
Tyus Durant <todurant@ncdot.gov>
2026-01-15 10:53:02 -0500

    use client remove

--- components/sections/Hero.tsx
+++ components/sections/Hero.tsx
@@ -1,5 +1,3 @@
-"use client";
-import { useEffect, useState } from "react";
 import Image from "next/image";
 import { PiPlayFill } from "react-icons/pi";
 import { FaCalendarAlt } from "react-icons/fa";
